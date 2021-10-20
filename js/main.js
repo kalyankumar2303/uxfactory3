6085d685a7a77a6ecd7817c9301408cbd47489f3
Kalyan <kalyankumar2303@gmail.com>
2021-10-20 07:22:23 +0530

    Commiting all excersices of batch 3

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function myfunction() {
+   document.getElementById("demo").innerHTML = "Hello Javascript!";
+   document.getElementById("demo").style.fontSize = "30px";
+   document.getElementById("demo").style.color = "red";
+   document.getElementById("demo").style.backgroundColor = "yellow";
+}
